562c579e9b3ecba88bc8321c90e278ad1070955e
premascs <prema@scsanalytics.com>
2018-08-15 01:05:31 -0400

    Algorithms

---
+++ src/algo/LinearSearch/LinearSearch.java
@@ -0,0 +1,22 @@
+package algo.LinearSearch;
+
+public class LinearSearch {
+
+    public static void main(String[] args) {
+        int[] container = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(linearSearch(container,6));
+
+    }
+
+    //Search for the element in an array and return position of the element
+    public static int linearSearch(int[] a, int element){
+
+        for (int i=0;i<a.length;i++){
+            if(a[i] == element){
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
---
+++ src/algo/Sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+package algo.LinearSearch.Sorting;
+
+public class SelectionSort {
+    // O(n^2)
+    public static void main(String[] args) {
+
+        int[] b = selectionSort(new int[]{2,5,1,3,8,7,4});
+        for (int i=0;i<b.length;i++){
+            System.out.println(b[i]);
+        }
+    }
+
+    //find minimum and swap
+    public static int[] selectionSort(int[] a){
+        int min;
+        for (int i =0;i<a.length;i++){
+            min = i;
+            for (int j=i+1;j<a.length;j++){
+                if(a[j] < a[min]){
+                    min =j;
+                }
+            }
+            int temp = a[i];
+            a[i]=a[min];
+            a[min]=temp;
+        }
+        return a;
+    }
+}
