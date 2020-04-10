1d8850421fe2e3b7c67fef5875c6ec748fe17ca1
premagopu <premagopu7@gmail.com>
2020-04-09 22:45:19 -0400

    Fix project

--- src/ds/circularLinkedList/App.java
+++ src/ds/circularLinkedList/App.java
@@ -1,8 +1,5 @@
 package ds.circularLinkedList;
 
-
-import java.util.concurrent.Callable;
-
 public class App {
     public static void main(String[] args) {
       CircularLinkedList list = new CircularLinkedList();
